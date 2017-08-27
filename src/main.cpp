4bdca9d807fef52687b47265e28d657814cf71f6
Joe Januszkiewicz <joe.januszk@gmail.com>
2017-08-26 22:43:41 -0400

    moved ClosestWaypoint and NextWaypoint functions to utils

--- src/main.cpp
+++ src/main.cpp
@@ -30,50 +30,6 @@ string hasData(string s) {
   return "";
 }
 
-int ClosestWaypoint(double x, double y, vector<double> maps_x, vector<double> maps_y)
-{
-
-	double closestLen = 100000; //large number
-	int closestWaypoint = 0;
-
-	for(int i = 0; i < maps_x.size(); i++)
-	{
-		double map_x = maps_x[i];
-		double map_y = maps_y[i];
-		double dist = distance(x,y,map_x,map_y);
-		if(dist < closestLen)
-		{
-			closestLen = dist;
-			closestWaypoint = i;
-		}
-
-	}
-
-	return closestWaypoint;
-
-}
-
-int NextWaypoint(double x, double y, double theta, vector<double> maps_x, vector<double> maps_y)
-{
-
-	int closestWaypoint = ClosestWaypoint(x,y,maps_x,maps_y);
-
-	double map_x = maps_x[closestWaypoint];
-	double map_y = maps_y[closestWaypoint];
-
-	double heading = atan2( (map_y-y),(map_x-x) );
-
-	double angle = abs(theta-heading);
-
-	if(angle > pi()/4)
-	{
-		closestWaypoint++;
-	}
-
-	return closestWaypoint;
-
-}
-
 // Transform from Cartesian x,y coordinates to Frenet s,d coordinates
 vector<double> getFrenet(double x, double y, double theta, vector<double> maps_x, vector<double> maps_y)
 {
--- src/utils.hpp
+++ src/utils.hpp
@@ -78,3 +78,37 @@ vector<double> getCarXYPointFromGlobalXYPoint(double car_x, double car_y, double
     const double y = -xg_trans_c * sin(yaw_r) + yg_trans_c * cos(yaw_r);
     return { x, y };
 }
+
+int ClosestWaypoint(double x, double y, vector<double> maps_x, vector<double> maps_y) {
+    double closestLen = 100000; //large number
+    int closestWaypoint = 0;
+
+    for(int i = 0; i < maps_x.size(); i++) {
+        double map_x = maps_x[i];
+        double map_y = maps_y[i];
+        double dist = distance(x, y, map_x, map_y);
+        if (dist < closestLen) {
+            closestLen = dist;
+            closestWaypoint = i;
+        }
+    }
+
+    return closestWaypoint;
+}
+
+int NextWaypoint(double x, double y, double theta, vector<double> maps_x, vector<double> maps_y) {
+    int closestWaypoint = ClosestWaypoint(x, y, maps_x, maps_y);
+
+    double map_x = maps_x[closestWaypoint];
+    double map_y = maps_y[closestWaypoint];
+
+    double heading = atan2((map_y - y), (map_x - x));
+
+    double angle = abs(theta - heading);
+
+    if (angle > pi() / 4) {
+        closestWaypoint++;
+    }
+
+    return closestWaypoint;
+}
