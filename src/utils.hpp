f143f9e5d7319f742b9b06a10fa3874aff9f5de8
Joe Januszkiewicz <joe.januszk@gmail.com>
2017-08-27 00:15:08 -0400

    moved udacity-provided utilities into coord_utils.hpp

--- src/utils.hpp
+++ src/utils.hpp
@@ -1,17 +1,9 @@
 #include <math.h>
 #include <iostream>
+#include "coord_utils.hpp"
 #include "spline.h"
 
 using namespace std;
-
-// For converting back and forth between radians and degrees.
-constexpr double pi() { return M_PI; }
-double deg2rad(double x) { return x * pi() / 180; }
-double rad2deg(double x) { return x * 180 / pi(); }
-
-double distance(double x1, double y1, double x2, double y2) {
-    return sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
-}
 
 // In seconds
 constexpr double timestep() { return 0.02; }
@@ -78,40 +70,6 @@ vector<double> getCarXYPointFromGlobalXYPoint(double car_x, double car_y, double
     const double x = +xg_trans_c * cos(yaw_r) + yg_trans_c * sin(yaw_r);
     const double y = -xg_trans_c * sin(yaw_r) + yg_trans_c * cos(yaw_r);
     return { x, y };
-}
-
-int ClosestWaypoint(double x, double y, vector<double> maps_x, vector<double> maps_y) {
-    double closestLen = 100000; //large number
-    int closestWaypoint = 0;
-
-    for(int i = 0; i < maps_x.size(); i++) {
-        double map_x = maps_x[i];
-        double map_y = maps_y[i];
-        double dist = distance(x, y, map_x, map_y);
-        if (dist < closestLen) {
-            closestLen = dist;
-            closestWaypoint = i;
-        }
-    }
-
-    return closestWaypoint;
-}
-
-int NextWaypoint(double x, double y, double theta, vector<double> maps_x, vector<double> maps_y) {
-    int closestWaypoint = ClosestWaypoint(x, y, maps_x, maps_y);
-
-    double map_x = maps_x[closestWaypoint];
-    double map_y = maps_y[closestWaypoint];
-
-    double heading = atan2((map_y - y), (map_x - x));
-
-    double angle = abs(theta - heading);
-
-    if (angle > pi() / 4) {
-        closestWaypoint++;
-    }
-
-    return closestWaypoint;
 }
 
 int getWaypointPrior(int waypoint, int total_waypoints) {
